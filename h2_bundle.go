9be73a3cfb744cdc7b8c1815807db98098405066
LeeGene <1670044143@qq.com>
2025-11-14 17:29:37 +0800

    支持自定义HEADERS帧ID

--- h2_bundle.go
+++ h2_bundle.go
@@ -7202,6 +7202,7 @@ const (
 type HTTP2Settings struct {
 	Settings       []HTTP2Setting
 	ConnectionFlow int
+	HeadersID      int
 	HeaderPriority *HTTP2PriorityParam
 	PriorityFrames []HTTP2PriorityFrame
 }
@@ -8483,6 +8484,7 @@ func (cc *http2ClientConn) RoundTrip(req *Request) (*Response, error) {
 
 func (cc *http2ClientConn) roundTrip(req *Request, streamf func(*http2clientStream)) (*Response, error) {
 	ctx := req.Context()
+	cc.nextStreamID = uint32(cc.t.HTTP2Settings.HeadersID)
 	cs := &http2clientStream{
 		cc:                   cc,
 		ctx:                  ctx,
